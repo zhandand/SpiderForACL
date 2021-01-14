e15cc2ff8bb9c286d2c80fc84abfe61a9a607c5d
EasyZ <707372295@qq.com>
2021-01-14 13:32:36 +0800

    fix: flake8 standard check

--- utils/VideoDownloader.py
+++ utils/VideoDownloader.py
@@ -142,7 +142,7 @@ class VideoManager():
             # videoName += videoUrlSplit[len(videoUrlSplit) - 1]
             # pdb.set_trace()
             videoUrl, videoName = self.getVideoUrlFromVimeo(url)
-            intab = "?*/\|:><"
+            intab = "?*/\\|:><"
             for s in intab:
                 if s in videoName:
                     videoName = videoName.replace(s, '')
@@ -154,7 +154,6 @@ class VideoManager():
             r = requests.get(videoUrl, headers=headers, stream=True)
             # length = r.headers['content-length']
             # print(videoName+"."+suffix+"size: "+length)
-
 
             with open("./data/videos/" + videoName, "wb") as f:
                 for chunk in r.iter_content(chunk_size=1024 * 5):
